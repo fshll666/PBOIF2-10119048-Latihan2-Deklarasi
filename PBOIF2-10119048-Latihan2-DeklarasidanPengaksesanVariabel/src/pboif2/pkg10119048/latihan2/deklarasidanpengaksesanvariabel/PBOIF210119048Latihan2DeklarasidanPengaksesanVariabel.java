4041ad4f86d8f7d658c380c8cf8ec0b49050b0e9
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-10-17 23:03:14 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan2-DeklarasidanPengaksesanVariabel/src/pboif2/pkg10119048/latihan2/deklarasidanpengaksesanvariabel/PBOIF210119048Latihan2DeklarasidanPengaksesanVariabel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan2.deklarasidanpengaksesanvariabel;
+
+/**
+ *
+ * @author 
+ * Nama      : Mochammad Faishal
+ * Kelas     : IF2
+ * NIM       : 10119048
+ * Deskripsi : Berisi Deklarasi dan Pengaksesan Variable
+ */
+public class PBOIF210119048Latihan2DeklarasidanPengaksesanVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
